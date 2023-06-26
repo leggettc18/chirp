26f4d547e4ba22593e3e0556020f5259ee32794c
Christopher Leggett <chris@leggett.dev>
2023-06-26 17:20:46 -0400

    📨 Allows posting + formatting changes 📨

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,7 +1,7 @@
 import { clerkClient } from "@clerk/nextjs/server";
 import type { User } from "@clerk/nextjs/api"
 import { z } from "zod";
-import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { createTRPCRouter, privateProcedure, publicProcedure } from "~/server/api/trpc";
 import { TRPCError } from "@trpc/server";
 
 const filterUserForClient = (user: User) => {
@@ -12,6 +12,11 @@ export const postsRouter = createTRPCRouter({
     getAll: publicProcedure.query(async ({ ctx }) => {
         const posts = await ctx.prisma.post.findMany({
             take: 100,
+            orderBy: [
+                {
+                    createdAt: "desc"
+                }
+            ]
         });
 
         const users = (
@@ -21,7 +26,6 @@ export const postsRouter = createTRPCRouter({
             })
         ).map(filterUserForClient);
 
-        console.log(users);
         return posts.map((post) => {
             const author = users.find((user) => user.id === post.authorId);
 
@@ -36,4 +40,19 @@ export const postsRouter = createTRPCRouter({
             }
         });
     }),
+    create: privateProcedure
+        .input(
+            z.object({
+                content: z.string().emoji().min(1).max(280),
+            })
+        ).mutation(async ({ ctx, input }) => {
+            const authorId = ctx.userId;
+            const post = await ctx.prisma.post.create({
+                data: {
+                    authorId,
+                    content: input.content,
+                },
+            });
+            return post;
+        }),
 });
