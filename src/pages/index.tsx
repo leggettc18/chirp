b22272b9fab47b247baa971f70dd8b1ce0184231
Christopher Leggett <chris@leggett.dev>
2023-06-27 11:37:03 -0400

    🍞 Toasts, Ratelimits, Enter to Post 🍞

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,8 +4,9 @@ import { type RouterOutputs, api } from "~/utils/api";
 import { SignInButton, useUser } from "@clerk/nextjs";
 import dayjs from "dayjs";
 import relativeTime from "dayjs/plugin/relativeTime"
-import { LoadingPage } from "~/components/loading";
+import { LoadingPage, LoadingSpinner } from "~/components/loading";
 import { useState } from "react";
+import toast from "react-hot-toast";
 dayjs.extend(relativeTime);
 
 const CreatePostWizard = () => {
@@ -19,6 +20,14 @@ const CreatePostWizard = () => {
         onSuccess: () => {
             setInput("");
             void ctx.posts.getAll.invalidate();
+        },
+        onError: (e) => {
+            const errorMessage = e.data?.zodError?.fieldErrors.content;
+            if (errorMessage && errorMessage[0]) {
+                toast.error(errorMessage[0]);
+            } else {
+                toast.error("Failed to post! Please try again later...");
+            }
         }
     });
 
@@ -34,9 +43,25 @@ const CreatePostWizard = () => {
             type="text"
             value={input}
             onChange={(e) => setInput(e.target.value)}
+            onKeyDown={(e) => {
+                if (e.key === "Enter") {
+                    e.preventDefault();
+                    if (input != "") {
+                        mutate({ content: input });
+                    }
+                }
+            }}
             disabled={isPosting}
         />
-        <button onClick={() => mutate({ content: input })}>Post</button>
+        {input !== "" && !isPosting && (
+            <button onClick={() => mutate({ content: input })} disabled={isPosting}>Post</button>
+        )}
+
+        {isPosting && (
+            <div className="flex justify-center items-center">
+                <LoadingSpinner size={20} />
+            </div>
+        )}
     </div>
 };
 
--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -3,10 +3,18 @@ import type { User } from "@clerk/nextjs/api"
 import { z } from "zod";
 import { createTRPCRouter, privateProcedure, publicProcedure } from "~/server/api/trpc";
 import { TRPCError } from "@trpc/server";
+import { Ratelimit } from "@upstash/ratelimit";
+import { Redis } from "@upstash/redis";
 
 const filterUserForClient = (user: User) => {
     return { id: user.id, username: user.username, profileImageUrl: user.profileImageUrl }
 }
+
+const ratelimit = new Ratelimit({
+    redis: Redis.fromEnv(),
+    limiter: Ratelimit.slidingWindow(3, "1 m"),
+    analytics: true
+});
 
 export const postsRouter = createTRPCRouter({
     getAll: publicProcedure.query(async ({ ctx }) => {
@@ -43,10 +51,12 @@ export const postsRouter = createTRPCRouter({
     create: privateProcedure
         .input(
             z.object({
-                content: z.string().emoji().min(1).max(280),
+                content: z.string().emoji("Only emojis are allowed!").min(1).max(280),
             })
         ).mutation(async ({ ctx, input }) => {
             const authorId = ctx.userId;
+            const { success } = await ratelimit.limit(authorId);
+            if (!success) throw new TRPCError({ code: "TOO_MANY_REQUESTS" });
             const post = await ctx.prisma.post.create({
                 data: {
                     authorId,
